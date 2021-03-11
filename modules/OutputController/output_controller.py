47b7161dfb59cadc6bd7bdbf8a0c115f84eb64a9
Azaze1l <56613711+Azaze1l@users.noreply.github.com>
2021-03-11 23:53:19 +0300

    io troubles
    решаю проблемы с показом перевода
    
    пока костыльно ввел хоткей нажав на который можно прекратить показ уведомлений и вернуться в режим отлова нового словечка
    
    ну и как ачивка я познакомился с мультипроцессингом...

--- modules/OutputController/output_controller.py
+++ modules/OutputController/output_controller.py
@@ -1,6 +1,10 @@
 """
 TODO:
 разобраться с IO задачей отлова хоткеев и настроить выход из цикла уведомлений при достаточном ознакомлении с переводом
+    1)после завершения процесса уничтожать уведомление
+        1.1 наверное наследоваться от класса ToastNotifier...
+    2)добавить в cancel функцию отчет времени после которого возвращается False чтобы выйти из корутины
+    или получше подумать над логикой...
 """
 
 import pdfkit
@@ -8,25 +12,23 @@ from win10toast import ToastNotifier
 import asyncio
 from modules.Wrapper import wrapper
 import pyperclip
+from modules.Wrapper import wrapper
+import concurrent.futures
 import keyboard
-
+import multiprocessing
+import time
 cfg = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
 
+tn = ToastNotifier()
 
-async def notifier(synonyms, meanings, word):
-    tn = ToastNotifier()
+
+def notify_spammer(synonyms, meanings, word):
     if len(synonyms) == 0:
         for i in range(3):
-            if keyboard.is_pressed('Ctrl + C + X'):
-                print('switched')
-                await wrapper.hotkey_catcher()
+            try:
+                tn.show_toast(title=word, msg=meanings[i], duration=7)
+            except IndexError:
                 break
-            else:
-                try:
-
-                    tn.show_toast(title=pyperclip.paste(), msg=meanings[i], duration=7)
-                except IndexError:
-                    break
     else:
         title = ''
         msg = ''
@@ -37,5 +39,27 @@ async def notifier(synonyms, meanings, word):
                 msg += meanings[i] + '\n'
             except IndexError:
                 break
-        tn.show_toast(title=title, msg=msg, duration=20)
+        tn.show_toast(title=title, msg=msg, duration=15)
+
+
+def notify_killer():
+    while True:
+        if keyboard.is_pressed('Ctrl + Z'):
+            break
+        time.sleep(0.2)
+    return True
+
+
+async def notifier(synonyms, meanings, word):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        canceler = executor.submit(notify_killer)
+        showing_func = multiprocessing.Process(target=notify_spammer, args=(synonyms, meanings, word))
+        showing_func.start()
+        print(canceler.result())
+        if canceler.result():
+            print('yep im switching')
+            showing_func.terminate()
+            showing_func.kill()
+
+
 
